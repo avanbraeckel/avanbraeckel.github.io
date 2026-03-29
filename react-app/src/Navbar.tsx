9f7974ea6f22c8ddea1d3408d87be30c2e64c33f
Austin Van Braeckel <ho.yee22vb@gmail.com>
2026-03-29 10:25:58 -0400

    feature: use Material UI AppBar/Drawer responsive navbar

--- react-app/src/Navbar.tsx
+++ react-app/src/Navbar.tsx
@@ -1,30 +1,75 @@
 import React from 'react';
-import styled from 'styled-components';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import { useState } from 'react';
 
-const Bar = styled.nav`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 50px;
-  background-color: #282c34;
-  display: flex;
-  align-items: center;
-  padding: 0 16px;
-  z-index: 1000;
-`;
+const Navbar: React.FC = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const [open, setOpen] = useState(false);
 
-const Brand = styled.a`
-  color: #fff;
-  text-decoration: none;
-  font-size: 1rem;
-  font-weight: bold;
-`;
+  const toggleDrawer = (value: boolean) => () => {
+    setOpen(value);
+  };
 
-const Navbar: React.FC = () => (
-  <Bar>
-    <Brand href="/">Home</Brand>
-  </Bar>
-);
+  const menuItems = [
+    { text: 'Home', href: '/' },
+  ];
+
+  const drawer = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {menuItems.map((item) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton component="a" href={item.href}>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          {isMobile && (
+            <IconButton color="inherit" edge="start" onClick={toggleDrawer(true)} sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+          )}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+          {!isMobile && menuItems.map((item) => (
+            <Typography
+              key={item.text}
+              component="a"
+              href={item.href}
+              sx={{ color: '#fff', textDecoration: 'none', ml: 2 }}
+            >
+              {item.text}
+            </Typography>
+          ))}
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+        {drawer}
+      </Drawer>
+    </Box>
+  );
+};
 
 export default Navbar;
